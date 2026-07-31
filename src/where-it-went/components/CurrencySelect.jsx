949b5e1eaf9cda85a6c2397f4aad774f9bf1569d
Gabe <jormania@gmail.com>
2026-08-01 00:33:25 +0300

    style(where-it-went): remove CurrencyFlag from CurrencySelect button to save horizontal space

--- src/where-it-went/components/CurrencySelect.jsx
+++ src/where-it-went/components/CurrencySelect.jsx
@@ -31,7 +31,7 @@ export function CurrencySelect({ id, value, onChange, currencies, style, disable
           ...style
         }}
       >
-        {value && <CurrencyFlag currency={value} style={{ marginRight: '6px' }} />}
+
         {value}
         <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" style={{ flexShrink: 0, marginLeft: '4px' }}><path d="m6 9 6 6 6-6"/></svg>
       </button>
